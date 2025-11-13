a44153a81be703f011e2872ff4c80bf86fee6814
aHighHooman <aHoomanOG@gmail.com>
2025-11-13 05:53:46 -0800

    Small Matrix Multiplier Modification 1. Completed Parametrization of the code for easier configuration

--- matrixMultiplierSmall.sv
+++ matrixMultiplierSmall.sv
@@ -1,9 +1,39 @@
-module matrixMultiplierSmall (
+module matrixMultiplierSmall #(
+    parameter int WIDTH = 6,
+    parameter int N = 3
+)(
     input  logic        clk,
     input  logic        rst_n,
-    input  logic signed [15:0] matrixA [3][3],
-    input  logic signed [15:0] matrixB [3][3],
-    output logic signed [31:0] resultMatrix [3][3]
+    input  logic signed [WIDTH-1:0] matrixA [N][N],
+    input  logic signed [WIDTH-1:0] matrixB [N][N],
+    output logic signed [2*WIDTH-1:0] resultMatrix [N][N]
 );
+    
+    // Needed for internal connections, sadly looks like the synthesizer isn'st smart enough to optimize these away
+    logic signed [WIDTH-1:0] rowData [N];
+    logic signed [WIDTH-1:0] colData [N];
+    
+    dataOrchestrator #(
+        .WIDTH(WIDTH),
+        .N(N)
+    ) dataOrch (
+        .clk      (clk),
+        .rst_n    (rst_n),
+        .matrixA  (matrixA),
+        .matrixB  (matrixB),
+        .row      (rowData),
+        .col      (colData)
+    );
+
+    systolicArray #(
+        .WIDTH(WIDTH),
+        .N(N)
+    ) systolicArr (
+        .clk          (clk),
+        .rst_n        (rst_n),
+        .row          (rowData),
+        .col          (colData),
+        .resultMatrix (resultMatrix)
+    );
     
 endmodule
