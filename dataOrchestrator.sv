19eeac2588d201e616512c840c65f95734f74479
aHighHooman <aHoomanOG@gmail.com>
2025-11-13 05:40:53 -0800

    Data Orchestrator Modification 1. Parametrized the code for easier configuration 2. Cleaned up/ Organized Code for better readability

--- dataOrchestrator.sv
+++ dataOrchestrator.sv
@@ -1,41 +1,43 @@
-module dataOrchestrator (
+module dataOrchestrator #(
+    parameter int WIDTH = 16,
+    parameter int N = 3
+)(
     input  logic        clk,
     input  logic        rst_n,
-    input  logic signed [15:0] matrixA [3][3],
-    input  logic signed [15:0] matrixB [3][3],
-    output logic signed [15:0] rowOne        ,
-    output logic signed [15:0] rowTwo        ,
-    output logic signed [15:0] rowThree      ,
-    output logic signed [15:0] colOne        ,
-    output logic signed [15:0] colTwo        ,
-    output logic signed [15:0] colThree
+    input  logic signed [WIDTH-1:0] matrixA [N][N],
+    input  logic signed [WIDTH-1:0] matrixB [N][N],
+    output logic signed [WIDTH-1:0] row     [N]   ,
+    output logic signed [WIDTH-1:0] col     [N]   
 );
 
-logic [3:0] counter;
+logic [N:0] counter;
 
+// Not fully parameterized yet, for N=3 only
 always_ff @(posedge clk) begin
+    
+    // Set all outputs to 0 on reset
     if (!rst_n) begin
-        rowOne   <= 16'sd0;
-        rowTwo   <= 16'sd0;
-        rowThree <= 16'sd0;
-        colOne   <= 16'sd0;
-        colTwo   <= 16'sd0;
-        colThree <= 16'sd0;
         counter  <= 4'd0;
+        for (int i = 0; i < N; i = i + 1) begin
+            row[i] <= 0;
+            col[i] <= 0;
+    end
+    
+    // Begin Data Orchestration
     end else begin
-        counter <= counter + 4'd1;
+        counter <= counter + 1;
 
-        if (counter >= 4'd0 && counter < 4'd3) begin
-            rowOne  <= matrixA[0][counter];
-            colOne  <= matrixB[counter][0];
+        if (counter >= 0 && counter < 3) begin
+            row[0]  <= matrixA[0][counter];
+            col[0]  <= matrixB[counter][0];
         end 
-        if (counter >= 4'd1 && counter < 4'd4) begin
-            rowTwo  <= matrixA[1][counter - 4'd1];
-            colTwo  <= matrixB[counter - 4'd1][1];
+        if (counter >= 1 && counter < 4) begin
+            row[1]  <= matrixA[1][counter - 1];
+            col[1]  <= matrixB[counter - 1][1];
         end
-        if (counter >= 4'd2 && counter < 4'd5) begin
-            rowThree <= matrixA[2][counter - 4'd2];
-            colThree <= matrixB[counter - 4'd2][2];
+        if (counter >= 2 && counter < 5) begin
+            row[2] <= matrixA[2][counter - 2];
+            col[2] <= matrixB[counter - 2][2];
         end
         
     end
