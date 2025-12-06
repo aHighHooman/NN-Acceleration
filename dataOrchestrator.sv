95879918ff62a1786cace6ce69e2d437ed1536cd
aHighHooman <aHoomanOG@gmail.com>
2025-12-05 21:53:02 -0800

    Data Orchestrator Modification 1. Modified Data Orchestrator to now work with a particular input data    flow from multiple FIFOs

--- dataOrchestrator.sv
+++ dataOrchestrator.sv
@@ -4,51 +4,60 @@ module dataOrchestrator #(
 )(
     input  logic        clk,
     input  logic        rst_n,
-    input  logic signed [WIDTH-1:0] matrixA [N][N],
-    input  logic signed [WIDTH-1:0] matrixB [N][N],
-    output logic signed [WIDTH-1:0] row     [N]   ,
-    output logic signed [WIDTH-1:0] col     [N]   
+    input  logic signed [WIDTH-1:0] colA    [N],
+    input  logic signed [WIDTH-1:0] rowB    [N],
+    output logic signed [WIDTH-1:0] row     [N],
+    output logic signed [WIDTH-1:0] col     [N],   
+    output logic        [7:0]       addrA   [N],
+    output logic        [7:0]       addrB   [N]
 );
 
 logic [N:0] counter;
 
-// Not fully parameterized yet, for N=3 only
+
 always_ff @(posedge clk) begin
     
     // Set all outputs to 0 on reset
     if (!rst_n) begin
-        counter  <= 4'd0;
+        counter  <= 0;
         for (int i = 0; i < N; i = i + 1) begin
+            // Set outputs to 0
             row[i] <= 0;
             col[i] <= 0;
+            addrA[i] <= 25;
+            addrB[i] <= 25;
         end
+    
     
     // Begin Data Orchestration
     end else begin
-        counter <= counter + 1;
-
-        if (counter >= 0 && counter < 3) begin
-            row[0]  <= matrixA[0][counter];
-            col[0]  <= matrixB[counter][0];
-        end else begin 
-            row[0]  <= 0;
-            col[0]  <= 0;
-        end
-        if (counter >= 1 && counter < 4) begin
-            row[1]  <= matrixA[1][counter - 1];
-            col[1]  <= matrixB[counter - 1][1];
-        end else begin 
-            row[1]  <= 0;
-            col[1]  <= 0;
-        end
-        if (counter >= 2 && counter < 5) begin
-            row[2] <= matrixA[2][counter - 2];
-            col[2] <= matrixB[counter - 2][2];
-        end else begin 
-            row[2] <= 0;
-            col[2] <= 0;
-        end
+        // Call initial memory addresses
         
+        counter <= counter + 1;
+        if (counter == 0) begin
+            for (int i = 0; i < N; i = i + 1) begin
+                addrA[i] <= i * N;
+                addrB[i] <= i;
+            end
+        end
+
+
+        for (int i = 0; i < N; i = i + 1) begin
+            if (counter >= i+1 && counter < i + N + 1) begin
+                addrA[i] <= addrA[i] + 1;
+                addrB[i] <= addrB[i] + N;
+            end
+            
+            if (counter >= i+2 && counter < i + N+2) begin
+                // Send the row and column data to the systolic array
+                row[i]  <= colA[i];
+                col[i]  <= rowB[i];
+                
+            end else begin 
+                row[i]  <= 0;
+                col[i]  <= 0;
+            end
+        end
     end
 end
 
